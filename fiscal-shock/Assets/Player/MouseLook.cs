6bb468a2b323f2ba633f0b131cc93de27ab68fbb
Williampsp1 <wlopez@unomaha.edu>
2020-02-09 18:10:15 -0600

    Added Player movement with model and gun

---
+++ fiscal-shock/Assets/Player/MouseLook.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    public Transform body;
+
+    float xRotation = 0f;
+    
+    // Cursor is locked to game
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Moves the camera with the mouse, uses Time.deltaTime for FPS correction (Independent of current Frame rate)
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+
+        xRotation -= mouseY;
+
+        //Cannot look further than 90 degrees up
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation,0f,0f);
+        body.Rotate(Vector3.up * mouseX);
+        
+    }
+}
