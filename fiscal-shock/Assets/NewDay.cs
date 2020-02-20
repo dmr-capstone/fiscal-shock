2bcbb971ff3f3818fa5abe3a020174dc9c0a3e8e
Zachary Maly <zalfenior@gmail.com>
2020-02-20 17:03:26 -0600

    Added in models for bad guys
    Two variants and textures/material for each. Also added a blaster, though it is not hooked up anywhere yet.

--- fiscal-shock/Assets/NewDay.cs
+++ fiscal-shock/Assets/NewDay.cs
@@ -7,7 +7,7 @@ public class NewDay : MonoBehaviour
     // to be triggered on arrival from the dungeon, accrues interest and causes hostility if not paid
     public bool startNewDay(bool mobNotPaid, bool bankNotPaid){
         if(mobNotPaid){
-            PlayerFinance.mobThreatLevel++;
+            PlayerFinance.sharkThreatLevel++;
         }
         if(bankNotPaid){
             PlayerFinance.bankThreatLevel++;
@@ -16,9 +16,9 @@ public class NewDay : MonoBehaviour
         PlayerFinance.debtBank += PlayerFinance.debtBank * PlayerFinance.bankInterestRate;
         ATMScript.bankDue = true;
         //Increase Mob loan by interest rate and reset variable if mob debt exists
-        if(PlayerFinance.debtMob > 0.0f){
-            PlayerFinance.debtMob += PlayerFinance.debtMob * PlayerFinance.mobInterestRate;
-            MobsterScript.mobDue = true;
+        if(PlayerFinance.debtShark > 0.0f){
+            PlayerFinance.debtShark += PlayerFinance.debtShark * PlayerFinance.sharkInterestRate;
+            MobsterScript.sharkDue = true;
         }
         return true;
     }
--- fiscal-shock/Assets/SharkScript.cs
+++ fiscal-shock/Assets/SharkScript.cs
@@ -6,14 +6,14 @@ public class MobsterScript : MonoBehaviour
 {
     //figure out how to import script interfaces
     //Also need threat increase when not paid, gets bad at 5 and really bad at 8
-    public static bool mobDue{get; set;} = false; //This is because the player starts with no debt to the mob
+    public static bool sharkDue{get; set;} = false; //This is because the player starts with no debt to the mob
 
     public bool addDebt(int amount){
-        if(PlayerFinance.mobThreatLevel < 5 && PlayerFinance.mobMaxLoan > (PlayerFinance.debtMob + amount)){
+        if(PlayerFinance.sharkThreatLevel < 5 && PlayerFinance.sharkMaxLoan > (PlayerFinance.debtShark + amount)){
             //mob threat is below 3 and is below max total debt
-            PlayerFinance.debtMob += amount;
+            PlayerFinance.debtShark += amount;
             PlayerFinance.cashOnHand += amount;
-            mobDue = true;
+            sharkDue = true;
             return true;
         } else {
             return false;
@@ -23,16 +23,16 @@ public class MobsterScript : MonoBehaviour
     public bool payDebt(int amount){
         if(PlayerFinance.cashOnHand < amount){//amount is more than money on hand
             return false;
-        } else if(PlayerFinance.debtMob < amount){ //amount is more than the debt
-            PlayerFinance.debtMob = 0.0f;//reduce debt to 0 and money on hand by the debt's value
-            PlayerFinance.cashOnHand -= PlayerFinance.debtMob;
-            mobDue = false;
+        } else if(PlayerFinance.debtShark < amount){ //amount is more than the debt
+            PlayerFinance.debtShark = 0.0f;//reduce debt to 0 and money on hand by the debt's value
+            PlayerFinance.cashOnHand -= PlayerFinance.debtShark;
+            sharkDue = false;
             return true;
         } else { //none of the above
             //reduce debt and money by amount
-            PlayerFinance.debtMob -= amount;
+            PlayerFinance.debtShark -= amount;
             PlayerFinance.cashOnHand -= amount;
-            mobDue = false;
+            sharkDue = false;
             return true;
         }
     }
