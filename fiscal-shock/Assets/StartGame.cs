c879abfe19e8ef43ee4553d0ac2754403fd75957
mturpen98 <mturpen98@gmail.com>
2020-02-24 16:06:31 -0600

    Play

---
+++ fiscal-shock/Assets/StartGame.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    public void MenuPlay() => SceneManager.LoadScene("Hub");
+
+}
