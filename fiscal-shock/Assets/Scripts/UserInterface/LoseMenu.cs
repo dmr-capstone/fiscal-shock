c56a0c3785591e0e6038000a255f05721cee2d76
mturpen98 <mturpen98@gmail.com>
2020-02-25 20:38:06 -0600

    Last 2 Scripts

---
+++ fiscal-shock/Assets/Scripts/UserInterface/LoseMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseMenu : MonoBehaviour
+{
+
+    public void Start(){
+        Debug.Log(SceneManager.GetActiveScene().buildIndex);
+        Debug.Log(SceneManager.GetActiveScene().name);
+    }
+
+    public void RetryClick (){
+        SceneManager.LoadScene("Hub");
+    }
+
+    public void BankruptClick (){
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+}
