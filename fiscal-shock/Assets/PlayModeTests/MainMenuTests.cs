1d9e8e31f180d0d06726c5f97330fdf6cdc142aa
pagewin <20141682+pagewin@users.noreply.github.com>
2020-03-07 15:17:44 -0600

    add most ci files and playmode tests

---
+++ fiscal-shock/Assets/PlayModeTests/MainMenuTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+namespace PlayModeTests {
+    public class MainMenuTests {
+        [UnityTest]
+        public IEnumerator testMainMenuPlayButton() {
+            // load the menu
+            SceneManager.LoadScene("Menu");
+            // wait 10s for menu load
+            yield return TestUtils.AssertSceneLoaded("Menu");
+            // should be at the main menu
+            Assert.AreEqual("Menu", SceneManager.GetActiveScene().name);
+            // click the play button
+            TestUtils.ClickButton("PlayButton");
+            // should load the hub within 10s
+            yield return TestUtils.AssertSceneLoaded("Hub");
+            Assert.AreEqual("Hub", SceneManager.GetActiveScene().name);
+        }
+
+        /*[Test]
+        public void testFail() { Assert.AreEqual(0, 1); }*/
+    }
+}
