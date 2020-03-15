636c931d3847bac974300ad3e523e8b9b21b4609
Zachary Maly <zalfenior@gmail.com>
2020-03-14 21:59:43 -0500

    Initial version
    Set up branch and add in a basic state manager. Specifically, adding in the loan list as they will be seperate.

---
+++ fiscal-shock/Assets/Scripts/Finance/StateManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loan
+{
+    float total {get; set;}
+    float rate {get; set;}
+    bool paid {get; set;}
+    bool source {get; set;}
+
+    //source is true when it is the shark, false when it is the bank
+    public void loan(float tot, float rat, bool yes, bool shark)
+    {
+        total = tot;
+        rate = rat;
+        paid = yes;
+        source = shark;
+    }
+}
+
+public class StateManager : MonoBehaviour
+{
+    //list of loans that the player posesses
+    public LinkedList<Loan> loanList = new LinkedList<Loan>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void calcCreditScore()
+    {
+
+    }
+
+}
