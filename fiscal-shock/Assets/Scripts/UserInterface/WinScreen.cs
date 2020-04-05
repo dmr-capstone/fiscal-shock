c9cb271a847b1487f2c3f6a1d3ddd3058338bfeb
mturpen98 <mturpen98@gmail.com>
2020-04-05 11:06:50 -0500

    Added Button and Function

--- fiscal-shock/Assets/Scripts/UserInterface/WinScreen.cs
+++ fiscal-shock/Assets/Scripts/UserInterface/WinScreen.cs
@@ -1,12 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinScreen : MonoBehaviour {
+    private GameObject loadingScreen;
+    private LoadingScreen loadScript;
     public void Start() {
         Settings.forceUnlockCursorState();
+        loadingScreen = GameObject.Find("LoadingScreen");
+        loadScript = (LoadingScreen)loadingScreen.GetComponent<LoadingScreen>();
     }
 
     public void QuitClick() {
         Debug.Log("Quit by win game.");
         Application.Quit();
     }
+
+    public void GoBack() {
+        Debug.Log("Reload into HUB");
+        //loadScript.startLoadingScreen("Hub");
+        SceneManager.LoadScene("Hub");
+        Time.timeScale = 1;
+        Settings.forceLockCursorState();
+    }
+
 }
