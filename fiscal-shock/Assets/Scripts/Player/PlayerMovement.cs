8fee8a60ba7ec9b4e3f8b27118d763e00fa37aab
Williampsp1 <wlopez@unomaha.edu>
2020-04-26 17:08:27 -0500

    Fixed documentation

--- fiscal-shock/Assets/Scripts/Player/PlayerMovement.cs
+++ fiscal-shock/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,91 +1,138 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
-/**
-Here is the tutorial I followed:
-Player Movement: https://www.youtube.com/watch?v=_QajrabyTJc&t=1s
+/// <summary>
+/// Here is the tutorial I followed:
+/// Player Movement: https://www.youtube.com/watch?v=_QajrabyTJc&t=1s
+/// Character Controller documentation: https://docs.unity3d.com/Manual/class-CharacterController.html
+/// </summary>
 
-Character Controller documentation: https://docs.unity3d.com/Manual/class-CharacterController.html
-*/
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 
-    //References to control the attributes of movement
+    /// <summary>
+    /// References to control the attributes of movement
+    /// </summary>
     public float speed = 12f;
     public float gravity = -50f;
     public float jumpBy = 1.5f;
 
-    //Reference to ground body that checks if it has touched a ground layer.
+    
+    /// <summary>
+    /// Reference to ground body that checks if it has touched a ground layer.
+    /// </summary>
     public Transform groundCheck;
     public float groundDistance = 0.4f;
 
-    //Reference to ground layer
+    /// <summary>
+    /// Reference to ground layer, obstacle and decoration masks
+    /// </summary>
     public LayerMask groundMask;
     public LayerMask obstacleMask;
     public LayerMask decorationMask;
 
-    //References for the keyboard input, check if you're touching the ground and velocity
+    /// <summary>
+    /// References for the keyboard input, check if you're touching the ground and velocity
+    /// </summary>
     public Vector3 velocity;
     bool isGrounded;
     public InputActionAsset inputActions;
 
-    //References to movement and jumping by player
+    /// <summary>
+    /// References to movement and jumping by player
+    /// </summary>
     private Vector2 movement;
     private bool jumping;
 
-    //Player gets the player input and performs the action. 
+    
+    /// <summary>
+    /// Assigns the input system configuration to the player's input controller
+    /// </summary>
+    /// <returns></returns>
     void Awake() {
         gameObject.GetComponent<PlayerInput>().actions = inputActions;
     }
 
-    //Player moves based off Input action
+
+    /// <summary>
+    /// Player moves based off Input action
+    /// </summary>
+    /// <param name="cont"></param>
+    /// <returns></returns>
     public void OnMovement(InputAction.CallbackContext cont) {
         movement = cont.ReadValue<Vector2>();
     }
 
-    //Player jumps based off Input action
+    /// <summary>
+    /// Player jumps based off Input action
+    /// </summary>
+    /// <param name="cont"></param>
+    /// <returns></returns>
     public void OnJump(InputAction.CallbackContext cont) {
         jumping = cont.phase == InputActionPhase.Performed;
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    /// <returns></returns>
     void Update()
     {
 
-        //Creates sphere around object to check if it has collided with a ground layer
+        /// <summary>
+        /// Creates sphere around object to check if it has collided with a ground layer
+        /// </summary>
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance,groundMask | obstacleMask | decorationMask);
-        //Resets velocity, so it doesnt go down forever
+
+        /// <summary>
+        /// Resets velocity, so it doesnt go down forever
+        /// </summary>
         if(isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
 
-        //Gets input from user
+        /// <summary>
+        /// Gets input from user
+        /// </summary>
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        //Creates direction where user wants to move
+        /// <summary>
+        /// Creates direction where user wants to move
+        /// </summary>
         Vector3 move = transform.right * x + transform.forward * z;
 
-        //Moves the player using move, speed and Time.deltaTime(Frame Rate independent)
+        ///<summary>
+        /// Moves the player using move, speed and Time.deltaTime(Frame Rate independent)
+        /// </summary>
         controller.Move(move * speed * Time.deltaTime);
 
-        //Only Jump if player is grounded, velocity.y brings player down
-        //if(Input.GetButtonDown("Jump") && isGrounded)
+        ///<summary>
+        /// Only Jump if player is grounded, velocity.y brings player down
+        /// </summary>
         if(jumping && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpBy * -2f * gravity);
         }
 
-        //Velocity for when user is falling down, using gravity and Time.deltaTime.
+        /// <summary>
+        /// Velocity for when user is falling down, using gravity and Time.deltaTime.
+        /// </summary>
         velocity.y += gravity * Time.deltaTime;
 
-        //Lets user fall down based on velocity
+        ///<summary>
+        /// Lets user fall down based on velocity
+        /// </summary>
         controller.Move(velocity * Time.deltaTime);
     }
 
-    //Teleports the player to a specific destination
+    /// <summary>
+    /// Teleports the player to a specific destination
+    /// </summary>
+    /// <param name="destination"></param>
+    /// <returns></returns>
     public void teleport(Vector3 destination) {
         controller.enabled = false;
         transform.position = destination;
